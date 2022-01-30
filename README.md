b9fe4101890520196c169972707428f88c22662d
Jogvan <jogvan@gmail.com>
2022-01-30 13:53:59 +0000

    Add win condition for the game after winning 5 rounds. Add functionality to restart game after a player has won. Changed EventListeners so it is not possible to click while game is not in progress

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rock-paper-scissors
-A Rock Paper Scissors game
+A Rock Paper Scissors game made with HTML/CSS/JS
+
+Press start game to start a new game. The player selects a hand and AI selects one randomly from array and the JavaScript decides who won. After one player wins 5 rounds the game ends. Restart game by pressing start game again.
+
+###Ideas for future features
+
+- Add multiplayer
+- Make AI not random based on previous rounds
--- rps.js
+++ rps.js
@@ -7,33 +7,44 @@ const startButton = document.querySelector(".start-game");
 const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
+const playerHand = document.querySelector(".player")
+const aiHand = document.querySelector(".ai");
+const textResult = document.querySelector(".result h2");
+const selectionButtons = document.querySelectorAll(".player-options button");
+const AIScore = document.querySelector(".AI-score");
+const playerScore = document.querySelector(".player-score");
 
-//Event Listeners
-startButton.addEventListener("click", () => { startGame() });
-rockButton.addEventListener("click", () => { playRound("Rock") })
-paperButton.addEventListener("click", () => { playRound("Paper") })
-scissorsButton.addEventListener("click", () => { playRound("Scissors") })
+startButton.addEventListener("click", startGame);
 
 //Enables hand buttons, disables start game
 function startGame() {
+    resetGame();
     divStartGame.setAttribute("style", "visibility: hidden;");
     divPlayerOptions.classList.add("game-running");
     divPlayerOptions.classList.remove("wait-for-start");
+    selectionButtons.forEach(btn => btn.addEventListener("click", playRound));
 }
-//Disables hand buttons, enabled start game
-//TODO Print final score and declare winner
-function endGame() {
+//Disables hand buttons, enable start game button
+function endGame(winner) {
     divStartGame.setAttribute("style", "visibility: visible;")
     divPlayerOptions.classList.remove("game-running");
     divPlayerOptions.classList.add("wait-for-start");
+    selectionButtons.forEach(btn => btn.removeEventListener("click", playRound));
+    textResult.textContent = `${winner} wins the game!`
 }
 
 //Receives input from the selected button, selects random hand for AI and decides who wins.
-function playRound(playerSelection) {
+function playRound(event) {
+    let playerSelection = event.target.value;
     let AISelection = randomHand();
-    
+
+    updateHandIcon(playerSelection, playerHand);
+    updateHandIcon(AISelection, aiHand);
+
     if (playerSelection === AISelection) {
-        console.log("It's a draw. Try again!")
+        textResult.textContent = `Both players picked ${playerSelection}`;
+        aiHand.setAttribute("style", "background: white")
+        playerHand.setAttribute("style", "background: white")
     }
     else if ((playerSelection === "Rock" && AISelection === "Scissors") ||
         (playerSelection === "Paper" && AISelection === "Rock") ||
@@ -44,41 +55,58 @@ function playRound(playerSelection) {
         AIWins();
     }
 }
+//Change icon of the target player to the selected hand
+function updateHandIcon(hand, target) {
+    let icon;
 
-function playRound2() {
-    playerWinCount = 0;
-    computerWinCount = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerHand = prompt("Input Rock, Paper or Scissors");
-        let computerHand = formHand();
-        playerHand = capitalize(playerHand);
-        console.log("Player selected: " + playerHand);
-        console.log("Computer selected: " + computerHand);
-
-        if (playerHand === computerHand) {
-            console.log("It's a draw. Try again!")
-            i--;
-        }
-        else if (!hand.includes(playerHand)) {
-            console.log("Invalid input. Make sure you spell it correctly");
-            i--;
-        }
-        else if ((playerHand === "Rock" && computerHand === "Scissors") ||
-            (playerHand === "Paper" && computerHand === "Rock") ||
-            (playerHand === "Scissors" && computerHand === "Paper")) {
-            playerWins(i);
-        }
-        else {
-            computerWins(i);
-        }
+    switch (hand) {
+        case "Rock":
+            icon = "✊";
+            break;
+        case "Paper":
+            icon = "🤚";
+            break;
+        case "Scissors":
+            icon = "✌";
+            break;
     }
-    if (playerWinCount > computerWinCount) {
-        console.log("Player wins with " + playerWinCount + " wins against " + computerWinCount);
-    }
-    else {
-        console.log("Computer wins with " + computerWinCount + " wins against " + playerWinCount);
-    }
+    target.textContent = icon;
 }
+
+// function playRound2() {
+//     playerWinCount = 0;
+//     computerWinCount = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let playerHand = prompt("Input Rock, Paper or Scissors");
+//         let computerHand = formHand();
+//         playerHand = capitalize(playerHand);
+//         console.log("Player selected: " + playerHand);
+//         console.log("Computer selected: " + computerHand);
+
+//         if (playerHand === computerHand) {
+//             console.log("It's a draw. Try again!")
+//             i--;
+//         }
+//         else if (!hand.includes(playerHand)) {
+//             console.log("Invalid input. Make sure you spell it correctly");
+//             i--;
+//         }
+//         else if ((playerHand === "Rock" && computerHand === "Scissors") ||
+//             (playerHand === "Paper" && computerHand === "Rock") ||
+//             (playerHand === "Scissors" && computerHand === "Paper")) {
+//             playerWins(i);
+//         }
+//         else {
+//             computerWins(i);
+//         }
+//     }
+//     if (playerWinCount > computerWinCount) {
+//         console.log("Player wins with " + playerWinCount + " wins against " + computerWinCount);
+//     }
+//     else {
+//         console.log("Computer wins with " + computerWinCount + " wins against " + playerWinCount);
+//     }
+// }
 
 //Future TODO AI tactical selection?
 function randomHand() {
@@ -93,17 +121,37 @@ function capitalize(word) {
 
 function playerWins() {
     playerWinCount++;
-    // console.log("Player wins round " + (i + 1));
 
-    let playerScore = document.querySelector(".player-score");
+
     playerScore.textContent = playerWinCount;
-
+    playerHand.setAttribute("style", "background: green")
+    aiHand.setAttribute("style", "background: red")
+    textResult.textContent = "Player wins the round!"
+    if (playerWinCount == 5) {
+        endGame("Player");
+    }
 }
 
 function AIWins() {
     computerWinCount++;
-    // console.log("Computer wins round " + (i + 1));
 
-    let AIScore = document.querySelector(".AI-score");
+
     AIScore.textContent = computerWinCount;
+    playerHand.setAttribute("style", "background: red")
+    aiHand.setAttribute("style", "background: green")
+    textResult.textContent = "AI wins the round!"
+    if (computerWinCount == 5) {
+        endGame("AI");
+    }
+}
+
+function resetGame() {
+    playerWinCount = 0;
+    computerWinCount = 0;
+
+    playerScore.textContent = "0";
+    AIScore.textContent = "0";
+
+    aiHand.setAttribute("style", "background: white")
+    playerHand.setAttribute("style", "background: white")
 }
